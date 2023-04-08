3342adb86923661f8cc30a05c15e425f513e180a
pmanso <98373747+pmanso28@users.noreply.github.com>
2023-04-08 02:03:56 -0300

    fontes incluídas

--- janki/App.js
+++ janki/App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import { useState } from 'react';
+
 
 export default function App() {
+
   return (
+
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={{ fontSize: 30 }}>Platform Default</Text>
     </View>
   );
 }

30f1b385c1eb953e947f45cfadd48fdf8a3da014
pmanso <98373747+pmanso28@users.noreply.github.com>
2023-04-09 18:53:11 -0300

    Header done
    started implementing a split screen in the middle.

--- janki/App.js
+++ janki/App.js
@@ -1,14 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Image, FlatList} from 'react-native';
 import { useState } from 'react';
 
 
 export default function App() {
 
+  const [decks, setDecks] = useState([
+    {deck: 'Kanji 1', key: 1 },
+    {deck: 'Kanji 2', key: 2 },
+    {deck: 'Kanji 3', key: 3 },
+    {deck: 'Hiragana', key: 4 },
+    {deck: 'Katakana', key: 5 },
+    {deck: 'Vocab 1', key: 6 },
+    {deck: 'Vocab 2', key: 7 },
+    ])
+
   return (
 
     <View style={styles.container}>
-      <Text style={{ fontSize: 30 }}>Platform Default</Text>
+      <View style={styles.header}>
+        <Image
+        style={styles.tinyLogo}
+        source={require('./assets/smallcolorless.png')}
+        />
+        <Text style={styles.titleHome}>HOME</Text>
+      </View>
+      <View style={styles.ladoalado}>
+          <FlatList style={styles.lista}
+          numColumns={1}
+          keyExtractor={(item) => item.key}
+          data={decks}
+          renderItem={({item}) => (
+              <Text style={styles.quadrado}>{item.deck}</Text>
+          )}
+          /> 
+        <View style={styles.pesquisa}>
+        </View>
+      </View>
+
+      <View style={styles.botoes}>
+      </View>
     </View>
   );
 }
@@ -16,8 +47,64 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: 'column',
+    flexWrap: 'wrap',
+    maxWidth: '100%'
   },
+
+  header:{
+    //Disposição na tela
+    flexDirection: 'row',
+    alignItems: 'center',
+    //Header propriamente dito
+    width: '100%',
+    height: 80,
+    backgroundColor: 'rgba(241, 208, 189, 1)',
+  },
+
+  tinyLogo:{
+    width: 40,
+    height: 40,
+    resizeMode: 'contain', //define como é feita a alteração do tamanho
+  },
+
+  titleHome:{
+    fontSize: 24,
+    left: 130,
+  },
+
+  ladoalado: {
+    flex: 1,
+    flexDirection:'row',
+    maxWidth: '50%',
+
+  },
+
+  lista: { 
+    //Disposição na tela
+    backgroundColor: 'red',
+  },
+
+  quadrado:{
+    //Texto
+    textAlign: 'center',
+    fontSize: 24,
+
+    //Configuração das caixas
+    backgroundColor: 'lightblue',
+    width: 120,
+    height: 80,
+    paddingTop: 20,
+    marginTop:  20,
+    borderRadius: 8,
+    alignSelf: 'center'
+    },
+
+    pesquisa: {
+      backgroundColor: 'blue',
+    },
+
+    botoes: {
+      backgroundColor: 'purple',
+    },
 });
